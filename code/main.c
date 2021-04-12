39729643f54036597faa0cfd0205ff77bf68bf44
ritarded <ritalino7@hotmail.com>
2021-04-12 10:33:30 +0100

    fixed errors created due to merge

--- code/main.c
+++ code/main.c
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-/** 
- * @file main.c
- * @brief Este ficheiro contem a main do guiao 1.
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include "stack.h"
-#include "parser.h"
-
-int main(){
-	char line[10240];
-
-	assert( fgets(line, 10240, stdin) != NULL);
-	assert( line[strlen(line) - 1] == '\n'   );
-
-	parse(line);
-	return 0;
-=======
 /** 
  * @file main.c
  * @brief Este ficheiro contem a main do guiao 1.
@@ -45,5 +23,3 @@ int main(){
 
 	parse(line);
 	return 0;
->>>>>>> b433a1a47fd89e5e8ee164c878af69789843f139
-}
